72f0a4bfd9b6df3fa543bcd848e1417dd5fc525a
Bilge Kagan Ozbay <48964248+bkozbay@users.noreply.github.com>
2019-05-28 03:57:17 +0300

    BK

---
+++ FİNAL.R
@@ -0,0 +1,29 @@
+migros <- setRefClass(Class = "migros", fields = c("sube", "icecek", "yiyecek", "teknoloji", "bakim"),methods = list(
+  initialize = function(sube, icecek, yiyecek, teknoloji, bakim){
+    .self$sube <- sube
+    .self$icecek <- icecek
+    .self$yiyecek <- yiyecek
+    .self$teknoloji <- teknoloji
+    .self$bakim <- bakim
+  }))
+    show = function(){
+      cat("MIGROS:", sube, "\n",
+          "IÇECEK:", icecek, "|",
+          "YIYECEK", yiyecek, "|",
+          "TEKNOLOJI", teknoloji,"|",
+          "BAKIM", bakim,"|")
+    }
+  '%+%' <- function(x, ...){
+    UseMethod("koru", x)
+  }  
+  koru.migros <- function(a,b,c,d){
+    list (
+      ticecek = a$icecek + b$icecek + c$icecek + d$icecek,
+      tyiyeyecek = a$yiyecek + b$yiyecek + c$yiyecek + d$yiyecek,
+      tteknoloji = a$teknoloji + b$teknoloji + c$teknoloji + d$teknoloji,
+      tbakim = a$bakim + b$bakim + c$bakim + d$bakim
+    )
+  }
+beyazit <- migros$new("Beyazit", 3, 5, 7, 6)
+maltepe <- migros$new("zevler", 4, 15, 27, 36)
+beyazit %+% maltepe
